ac139e34e84aee7f25a36521cf352673c3acb16a
hanks <hanks6160@gmail.com>
2023-03-21 17:49:58 +0900

    문자에서 숫자를 추출해서 정수형으로 반환

---
+++ src/com/example/StringTest/StringTest09.java
@@ -0,0 +1,42 @@
+package com.example.StringTest;
+
+import java.util.Scanner;
+
+public class StringTest09 {
+
+    /**
+     * 아스키 코드를 이용해서
+     * @param str
+     * @return
+     */
+    public int solution(String str){
+       int answer=0;
+       for(char x : str.toCharArray()){
+           if(x>=48 && x<=57)
+               answer = answer*10+(x-48);
+       }
+        return answer;
+    }
+
+    /**
+     * Charter 클래스 이용
+     * @param s
+     * @return
+     */
+    public int solution2(String s){
+        String answer="";
+        for(char x : s.toCharArray()){
+            if(Character.isDigit(x)) // 숫자인지 확인
+                answer += x;
+        }
+        return Integer.parseInt( answer);
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        StringTest09 test09= new StringTest09();
+        System.out.println(test09.solution2(input));
+    }
+
+}
